455f987084f85583b864dee3dbcd755dcede0843
hiharibo <dev_harim@xinno.io>
2026-04-21 18:31:43 +0900

    feat : 상수 분리

--- src/components/StatusTitle.tsx
+++ src/components/StatusTitle.tsx
@@ -1,11 +1,10 @@
 "use client";
 
+import { DONE_TITLE, IN_PROGRESS_TITLE } from "../app/constants/constants";
+
 type StatusTitleProps = {
   isDone: boolean;
 };
-
-const DONE_TITLE = "완료";
-const IN_PROGRESS_TITLE = "진행중";
 
 function StatusTitle({ isDone }: StatusTitleProps) {
   const statusTitle = isDone ? DONE_TITLE : IN_PROGRESS_TITLE;
